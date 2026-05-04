56ac1adcc89485d3743626476b1967fb4b1df95e
Tarek Abdelrahman <tarekmohamed9868@gmail.com>
2026-05-04 14:35:13 +0300

    main.tf for the security module

---
+++ infra/terraform/modules/security/main.tf
@@ -0,0 +1,104 @@
+resource "aws_security_group" "public_alb_sg" {
+  name   = "${local.prefix}-public-alb-sg"
+  vpc_id = var.vpc_id
+ 
+  dynamic "ingress" {
+    for_each = local.alb_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${local.prefix}-public-alb-sg"
+    Team = lookup(var.extra_tags, "Team", "General")
+  }
+}
+
+resource "aws_security_group" "web_tier_sg" {
+  name   = "${local.prefix}-web-tier-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "internal_alb_sg" {
+  name   = "${local.prefix}-internal-alb-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web_tier_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "app_tier_sg" {
+  name   = "${local.prefix}-app-tier-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.internal_alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  count  = var.create_rds_sg ? 1 : 0
+  
+  name   = "${local.prefix}-rds-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app_tier_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
