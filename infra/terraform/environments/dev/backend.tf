99e34f31a69cc9b2dc59c464df062f1526531a21
Tarek Abdelrahman <tarekmohamed9868@gmail.com>
2026-05-05 11:57:35 +0300

    editing s3 bucket name

--- infra/terraform/environments/dev/backend.tf
+++ infra/terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-car-rent-terraform-state-bucket" # MUST exist in AWS first!
+    bucket         = "car-rent-terraform-state-bucket" # MUST exist in AWS first!
     key            = "dev/terraform.tfstate"             
     region         = "us-east-1"
     profile        = "demo"                               
